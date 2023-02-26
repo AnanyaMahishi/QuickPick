2584dc8edf1c145675826fd4d323f519c5d57cab
AayushNagarr <92391624+AayushNagarr@users.noreply.github.com>
2023-02-26 05:47:50 +0530

    aaa

--- index.js
+++ index.js
@@ -32,66 +32,51 @@ client.on('ready', () => {
 });
 
 async function startBot() {
-	// Connect to MongoDB
-	await mongoClient.connect();
-	console.log('Connected to MongoDB');
-  
-	// Select the database
-	const db = mongoClient.db(dbName);
-  
+    // Connect to MongoDB
+    await mongoClient.connect();
+    console.log('Connected to MongoDB');
+
+    // Select the database
+    const db = mongoClient.db(dbName);
+
 	function findObjectByName(arr, name) {
-	  for (let i = 0; i < arr.length; i++) {
-		if (arr[i].name === name) {
-		  return arr[i];
+		for (let i = 0; i < arr.length; i++) {
+		  if (arr[i].name === name) {
+			return arr[i];
+		  }
 		}
+		return null;
 	  }
-	  return null;
-	}
-  
-	// Listen for new messages
-	client.on('message', async (message) => {
-	  if (message.body === 'Hi') {
-		await client.sendMessage(
-		  message.from,
-		  'Hello, which vendor would you like to order from today? (type name) \n\nGJB\nPircube\nJuice Point'
-		);
-	  } else {
-		// Check if the message is from a user who started the ordering process
-		if (orderInProgress.hasOwnProperty(message.from)) {
-		  if (message.body === '/order') {
-			// Display the menu to the user
-			const vendor = orderInProgress[message.from];
-			await client.sendMessage(
-			  message.from,
-			  `Here's our menu:\n\n${vendor.menu
-				.map((item, index) => `${index + 1}. ${item.name} - $${item.price}`)
-				.join('\n')}\n\nPlease reply with the numbers of the items you want to order separated by commas (e.g. 1,3,4).`
-			);
-		  } else if (message.body.startsWith('/confirm')) {
-			// Confirm the order and calculate the total
-			const vendor = orderInProgress[message.from];
-			const orderItems = message.body
-			  .slice(8)
-			  .split(',')
-			  .map((item) => Number(item.trim()) - 1);
-			const items = orderItems.map((index) => vendor.menu[index]);
-			const total = items.reduce((acc, curr) => acc + curr.price, 0);
-			await client.sendMessage(
-			  message.from,
-			  `Great, you have ordered:\n\n${items
-				.map((item) => `${item.name} - $${item.price}`)
-				.join('\n')}\n\nYour total is $${total}. Please confirm your order by typing "Yes".`
-			);
-		  } else if (message.body.toLowerCase() === 'yes') {
-			// Confirm the order and save it to the database
-			const vendor = orderInProgress[message.from];
-			const orderItems = message.body
-			  .slice(8)
-			  .split(',')
-			  .map((item) => Number(item.trim()) - 1);
-			const items = orderItems.map((index) => vendor.menu[index]);
-			const total = items.reduce((acc, curr) => acc + curr.price, 0);
-  
+    // Listen for new messages
+    client.on('message', message => 
+    {
+		if(message.body === 'Hi') {
+			message.reply('Hello, which vendor would you like to order from today? (type name) \n\nGJB\nPircube\nJuice Point');
+		}
+		else
+		{	client.on('message', async(message) => 
+			{
+				
+				//let namesString = '';
+				vendor = findObjectByName(bigmenu.vendors, message.body);
+				console.log(vendor.name);
+				client.sendMessage(message.from, "Type /order to start ordering now, /confirm when you're done");
+				
+				// for (let i = 0; i < vendor.menu.length; i++) {
+				// 	namesString += vendor.menu[i].name + '\n';
+				//   }
+				//client.sendMessage(message.from, namesString);
+				client.on('message', async(message) => {
+
+					if(message.body === '/order')
+					{
+						await client.sendMessage(message.from, `Here's our menu:\n\n${vendor.menu.map((item, index) => `${index + 1}. ${item.name} - $${item.price}`).join('\n')}\n\nPlease reply with the numbers of the items you want to order separated by commas (e.g. 1,3,4).`);
+					}
+					else if(message.body.startsWith('/confirm')){
+						
+						const orderItems = message.body.slice(8).split(',').map(item => Number(item.trim()) - 1);
+						const items = orderItems.map(index => vendor.menu.[index]);
+						const total = items.reduce((acc, curr) => acc + curr.price, 0);
 						await client.sendMessage(message.from, `Great, you have ordered:\n\n${items.map(item => `${item.name} - $${item.price}`).join('\n')}\n\nYour total is $${total}. Please confirm your order by typing "Yes".`);
 						
 					}else if (message.body.toLowerCase() === 'yes') {
@@ -100,6 +85,7 @@ async function startBot() {
 					
 				})
 			});
+		}
 		}
 	}
 	)

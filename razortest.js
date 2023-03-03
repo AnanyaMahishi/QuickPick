db4f4d75a5ec95ceecd8bd3b14ce054cbda8daef
AayushNagarr <92391624+AayushNagarr@users.noreply.github.com>
2023-03-03 21:10:43 +0530

    asdf

--- razortest.js
+++ razortest.js
@@ -4,7 +4,7 @@ const Razorpay = require('razorpay');
 
 
 
-var instance = new Razorpay({
+var razorpay = new Razorpay({
     key_id: 'rzp_test_lwRQUtpiJFKv77',
     key_secret: 'SVdjEp6cQjDIead0Ann7ClIR',
   })
@@ -13,25 +13,33 @@ var instance = new Razorpay({
 
   var options = {
     
-    amount: 200,  // amount in the smallest currency unit
-    currency: "INR",
-    receipt: "order_rcptid_11"
+      amount: 200,
+      currency: "INR",
+      receipt: "awiouvfgaw",
+      payment_capture: 1,
+      notes: {
+          vendor_upi_id: "8095300313@paytm",
+      },
+ 
   };
 
-  if(true)
-  
-  {
-    instance.orders.create(options, function(err, order) {
-
-  
-    
-  
-    })
-  
-  }
 
 
-  
+ async function generatePaymentLink() {
+  // Create an order
+  const order = await razorpay.orders.create({
+    amount: 50000, // Amount in paise (Rs. 500)
+    currency: 'INR',
+    payment_capture: 1,
+  });
+
+  // Generate a payment link for the order
+  const paymentLink = `https://checkout.razorpay.com/v1/payment?order_id=${order.id}`;
+
+  console.log(`Payment link: ${paymentLink}`);
+}
+
+// Call the async function to generate the payment link
+generatePaymentLink();
 
 
-  //End of payment code

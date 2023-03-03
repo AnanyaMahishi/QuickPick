29c11d9815c767a2aa559630283f50ec7e77743d
AayushNagarr <92391624+AayushNagarr@users.noreply.github.com>
2023-03-03 23:15:13 +0530

    awf

--- razortest.js
+++ razortest.js
@@ -3,7 +3,7 @@ const Razorpay = require('razorpay');
 
 
 
-
+/*
 var razorpay = new Razorpay({
     key_id: 'rzp_test_lwRQUtpiJFKv77',
     key_secret: 'SVdjEp6cQjDIead0Ann7ClIR',
@@ -21,25 +21,55 @@ var razorpay = new Razorpay({
           vendor_upi_id: "8095300313@paytm",
       },
  
-  };
+  };*/
+
+var instance = new Razorpay({ key_id: 'rzp_test_lwRQUtpiJFKv77', key_secret: 'SVdjEp6cQjDIead0Ann7ClIR' })
+
+ const order = instance.paymentLink.create({
+  amount: 500,
+  currency: "INR",
+  accept_partial: true,
+  first_min_partial_amount: 100,
+  description: "For XYZ purpose",
+  customer: {
+    name: "Gaurav Kumar",
+    email: "gaurav.kumar@example.com",
+    contact: "+919000090000"
+  },
+  notify: {
+    sms: true,
+    email: true
+  },
+  reminder_enable: true,
+  notes: {
+    policy_name: "Jeevan Bima"
+  },
+  callback_url: "https://example-callback-url.com/",
+  callback_method: "get"
+}
+,(err, result)=>{
+  if(err) console.log(err);
+  console.log(result.short_url);
+})
 
 
 
- async function generatePaymentLink() {
-  // Create an order
+
+ /*async function generatePaymentLink() {
+
   const order = await razorpay.orders.create({
-    amount: 50000, // Amount in paise (Rs. 500)
+    amount: 50000,
     currency: 'INR',
     payment_capture: 1,
   });
 
-  // Generate a payment link for the order
   const paymentLink = `https://checkout.razorpay.com/v1/payment?order_id=${order.id}`;
 
   console.log(`Payment link: ${paymentLink}`);
 }
 
-// Call the async function to generate the payment link
 generatePaymentLink();
+*/
+
 
 

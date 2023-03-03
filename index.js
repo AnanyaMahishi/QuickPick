076e3e349c92e7f88eda9b2a83f99210de1aabb3
aayushsenapati <92628133+aayushsenapati@users.noreply.github.com>
2023-03-03 18:48:10 +0530

    yuss

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
-const qrcode = require('qrcode-terminal');
-const fs = require('fs');
+const qrcode = require("qrcode-terminal");
 // const bigmenu = require('./menu.json');
 const { MongoClient, ObjectId } = require("mongodb");
+const Razorpay = require("razorpay");
 
 // MongoDB connection URL and database name
-const mongoUrl = 'mongodb+srv://quickPick:quickPick@quickpick.kqhqbdn.mongodb.net/test';
-const dbName = 'foodOrders';
+const mongoUrl =
+    "mongodb+srv://quickPick:quickPick@quickpick.kqhqbdn.mongodb.net/test";
+const dbName = "foodOrders";
 
-userStore = {
-    
-}
-
+userStore = {};
+const razorpay = new Razorpay({
+    key_id: "rzp_test_lwRQUtpiJFKv77",
+    key_secret: "SVdjEp6cQjDIead0Ann7ClIR",
+});
 
 // Create a new MongoDB client
 const mongoClient = new MongoClient(mongoUrl, { useUnifiedTopology: true });
 
-const { Client } = require('whatsapp-web.js');
+const { Client } = require("whatsapp-web.js");
 
 const client = new Client();
 
 //const { MongoStore } = require('wwebjs-mongo');
 //const mongoose = require('mongoose');
 
-
-
-
-client.on('qr', qr => {
+client.on("qr", (qr) => {
     qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("ready", () => {
+    console.log("Client is ready!");
     startBot();
 });
 
 async function startBot() {
     // Connect to MongoDB
     await mongoClient.connect();
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
 
     // Select the database
     const db = mongoClient.db(dbName);
-    menu = db.collection("menu")
-    const bigmenu=await menu.findOne({})
-
-
-
+    menu = db.collection("menu");
+    const bigmenu = await menu.findOne({});
 
     function findObjectByName(arr, name) {
         for (let i = 0; i < arr.length; i++) {
@@ -56,60 +52,85 @@ async function startBot() {
         return null;
     }
     // Listen for new messages
-    
-        client.on('message', async message => {
-            if (message.body === 'Hi') {
-                
-                message.reply(`Pick a vendor :\n\n${bigmenu.vendors.map((single, index) => `${index + 1}. ${single.name}`).join('\n')}\n Use the command /pick followed by a number to pick the restaurant (eg. /pick 1,2,3)`);
+
+    client.on("message", async (message) => {
+        if (message.body === "Hi") {
+            message.reply(
+                `Pick a vendor :\n\n${bigmenu.vendors
+                    .map((single, index) => `${index + 1}. ${single.name}`)
+                    .join(
+                        "\n"
+                    )}\n Use the command /pick followed by a number to pick the restaurant (eg. /pick 1,2,3)`
+            );
+        } else if (message.body.startsWith("/pick")) {
+            const orderRes = Number(message.body.slice(5).trim() - 1);
+            const restaurant = bigmenu.vendors[orderRes].name;
+            const chatId = message.from;
+            userStore[chatId] = restaurant;
+            const vendor = findObjectByName(bigmenu.vendors, restaurant);
+            await client.sendMessage(
+                message.from,
+                `Here's our menu:\n\n${vendor.menu
+                    .map((item, index) => `${index + 1}. ${item.name} - ₹${item.price}`)
+                    .join(
+                        "\n"
+                    )}\n\nPlease reply with /confirm followed by the numbers of the items you want to order separated by commas (e.g. 1,3,4).`
+            );
+
+            client.sendMessage(
+                message.from,
+                "Your restaurant has been selected, type /confirm to start ordering now."
+            );
+        } else if (message.body.startsWith("/confirm")) {
+            const orderItems = message.body
+                .slice(8)
+                .split(",")
+                .map((item) => Number(item.trim()) - 1);
+            const chatId = message.from;
+            const vendor = findObjectByName(bigmenu.vendors, userStore[chatId]);
+            const items = orderItems.map((index) => vendor.menu[index]);
+            const total = items.reduce((acc, curr) => acc + curr.price, 0);
+            const date = new Date();
+            const time = date.toLocaleTimeString();
+            const foodReceipt = {
+                _id: new ObjectId(),
+                restaurant: userStore[chatId],
+                fooditems: items,
+                cost: total,
+                ordertime: time,
+            };
+            const paymentOptions = {
+                amount: total,
+                currency: "INR",
+                receipt: foodReceipt._id,
+                payment_capture: 1,
+                notes: {
+                    vendor_upi_id: vendor.upi,
+                },
+            };
+            const payment = razorpay.orders.create(paymentOptions);
+            const paymentLink = payment.short_url;
+
+            console.log("receipt that is uploaded to db", foodReceipt);
+            console.log("user state object", userStore);
+
+            await client.sendMessage(
+                message.from,
+                `Great, you have ordered:\n\n${items
+                    .map((item) => `${item.name} - ₹${item.price}`)
+                    .join("\n")}\n\nYour total is ₹${total}..`
+            );
+            await client.sendMessage(message.from, paymentLink);
+            const currPay = razorpay.payments.fetch(payment.id);
+
+            if (currPay.error === undefined) {
+                await db.collection("orders").insertOne(foodReceipt);
+                await client.sendMessage(
+                    message.from,
+                    "Your order has been confirmed! Thank you for choosing QuickPick."
+                );
             }
-            else if (message.body.startsWith('/pick')) {
-                
-                const orderRes = Number(message.body.slice(5).trim() - 1);
-                const restaurant = bigmenu.vendors[orderRes].name;
-                const chatId = message.from;
-                userStore[chatId] = restaurant;
-                vendor = findObjectByName(bigmenu.vendors, restaurant);
-                await client.sendMessage(message.from, `Here's our menu:\n\n${vendor.menu.map((item, index) => `${index + 1}. ${item.name} - ₹${item.price}`).join('\n')}\n\nPlease reply with /confirm followed by the numbers of the items you want to order separated by commas (e.g. 1,3,4).`);
-                
-                client.sendMessage(message.from, 'Your restaurant has been selected, type /confirm to start ordering now.');
-            }
-            else if (message.body.startsWith('/confirm')) {
-
-                const orderItems = message.body.slice(8).split(',').map(item => Number(item.trim()) - 1);
-                const items = orderItems.map(index => vendor.menu[index]);
-                const total = items.reduce((acc, curr) => acc + curr.price, 0);
-                const date = new Date();
-                const time = date.toLocaleTimeString();
-                const chatId = message.from;
-                let receipt = {
-                    _id: new ObjectId(),
-                    restaurant: userStore[chatId],
-                    fooditems: items,
-                    cost: total,
-                    ordertime: time
-                }
-
-                console.log(receipt);
-
-                console.log(userStore);
-                await client.sendMessage(message.from, `Great, you have ordered:\n\n${items.map(item => `${item.name} - ₹${item.price}`).join('\n')}\n\nYour total is ₹${total}. Please confirm your order by typing "Yes".`);
-                
-                await db.collection('orders').insertOne(receipt);
-                await client.sendMessage(message.from, 'Your order has been confirmed! Thank you for choosing QuickPick.');
-                
-            } /* else if (message.body.toLowerCase() === 'yes') {
-
-                const existingReceipt = await db.collection('orders').findOne({_id: receipt._id});
-                if (existingReceipt) {
-                    await db.collection('orders').updateOne({_id: receipt._id}, {$set: receipt});
-                } else {
-                    await db.collection('orders').insertOne(receipt);
-                }
-            
-                await client.sendMessage(message.from, 'Your order has been confirmed! Thank you for choosing QuickPick.');
-            } */
-
-        })
-
+        }
+    });
 }
 client.initialize();
